3b697330f21be1428a17073de5450b693e558d6c
Pirin <chuckdanorris123@gmail.com>
2022-06-01 23:40:51 -0400

    added newsagghandler and news aggpage

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
+
+NewsAggPage struct {
+	Title string
+	News string
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: "some news"}
+	t, _ := template.ParseFiles(basictemplating.html)
+	t.execute(w,p)
+}
 
 func indexhandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is so neat!</h1>")
@@ -11,5 +23,6 @@ func indexhandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg", newsAggHandler)
 	http.ListenAndServe(":8000", nil)
 }
