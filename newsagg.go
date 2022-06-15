fa64fe6cf05877526ae01122943e2a0ee471819d
pirin <chuckdanorris123@gmail.com>
2022-06-15 19:10:31 -0400

    added structs to main.go

--- newsagg.go
+++ newsagg.go
@@ -7,25 +7,10 @@ import (
 	"net/http"
 )
 
-type Sitemapindex struct {
-	Locations []string `xml:"sitemap>loc"` 
-}
-
-type News struct {
-	Titles []string `xml:"url>news>title"`
-	Keywords []stirng `xml: "url>news>keywords"`
-	Locations []string `xml:"url>loc"`
-}
-
-type NewsMap struct {
-	Keyword string
-	Location string
-}
-
-func main() {
+func Aggr() {
 	var s Sitemapindex
 	var n News
-	resp _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap)
@@ -35,13 +20,13 @@ func main() {
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
 
-		for idx, _ := range n.Keywords {
-			news_map[n.Titles[idx]] = NewsMap {n.Keywords[idx], n.Locations[idx]}
+		for idx := range n.Keywords {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		}
 	}
 	for idx, data := range news_map {
-		fmt.Println("\n\n\n\n\n",idx)
-		fmt.Println("\n",data.Keyword)
-		fmt.Println("\n",data.Location)
+		fmt.Println("\n\n\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
 	}
 }
