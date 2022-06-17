f556e842370fa896954dc54c66eb5411e673a545
piringraph <chuckdanorris123@gmail.com>
2022-06-16 23:28:49 -0400

    changed newsagg location and wrote the templates in basictemplating.go

--- newsagg/newsagg.go
+++ newsagg/newsagg.go
@@ -7,7 +7,22 @@ import (
 	"net/http"
 )
 
-func Aggr() {
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword  string
+	Location string
+}
+
+func main() {
 	var s Sitemapindex
 	var n News
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
