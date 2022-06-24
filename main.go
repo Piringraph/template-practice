b3415d3d24809015ed246f4886b6745894a0909d
pirin <chuckdanorris123@gmail.com>
2022-06-23 21:57:48 -0400

    deleted facebook.xml and restored main.go

--- main.go
+++ main.go
@@ -9,39 +9,50 @@ import (
 )
 
 type Url struct {
-	XMLName xml.Name `xml:"url"`
-	Url     []News   `xml:"news:news"`
-	Name    string
+	Keyword  string
+	Location string
+}
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]Url
+}
+
+type Sitemapindex struct {
+	Locations []string `xml:"url"`
 }
 
 type News struct {
-	XMLName xml.Name `xml:"news:news"`
-	Title   string   `xml:"news:title"`
-	Date    string   `xml:"news:publication_date"`
-}
-
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	var s Url
-	var n News
-	resp, _ := http.Get("https://www.washingtonpost.com/news-world-sitemap.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(bytes, &s)
-	for _,  := range s.Url {
-		resp, _ := http.Get("https://www.washingtonpost.com/news-world-sitemap.xml")
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-	}
-
-	var url Url
-	xml.Unmarshal(bytes, &url)
-
-	p := Url{Name: "Amazing News Aggregator", Url: []News{}}
-	t, _ := template.ParseFiles("newsaggtemplate.html")
-	t.Execute(w, p)
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	var s Sitemapindex
+	var n News
+	resp, _ := http.Get("https://www.washingtonpost.com/news-world-sitemap.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+	news_map := make(map[string]Url)
+
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+
+		for idx := range n.Keywords {
+			news_map[n.Titles[idx]] = Url{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+	t, _ := template.ParseFiles("newsaggtemplate.html")
+	t.Execute(w, p)
 }
 
 func main() {
